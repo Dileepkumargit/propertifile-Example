4f7d7010ac741150a628279f0829f05a73ba8e26
Dileepkumargit <thupatidileep@gmail.com>
2020-03-17 11:12:25 +0530

    secong commit

--- src/main/java/standerd/myntraLogin.java
+++ src/main/java/standerd/myntraLogin.java
@@ -6,7 +6,6 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -15,12 +14,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import GenericUtilities.Gneenric;
+import GenericUtilities.genericUtilities;;
 
-public class myntraLogin {
+public class myntraLogin extends genericUtilities{
+	public static WebDriver driver;
 	public static void main(String[] args) throws AWTException, IOException {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dhilip\\Downloads\\driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		 driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.myntra.com/");
 		Actions Action = new Actions(driver);
@@ -35,11 +37,22 @@ public class myntraLogin {
 	    obj.load(objfile);	
 		String username = "thupatidileep@gmail.com";
 		String password = "dileep335";
+//		waitforelement(By.xpath(obj.getProperty("usernamexapth")));
 		driver.findElement(By.xpath(obj.getProperty("usernamexapth"))).sendKeys(username);
 		System.out.println("User enter the username ::"+ username);
 		driver.findElement(By.xpath(obj.getProperty("passwordxapth"))).sendKeys(password);
 		System.out.println("User enter the password :: "+password);
+		waitforelement(By.xpath(obj.getProperty("clickOnLoginBtn")));
+//		waitforelement(By.xpath("//div//button[@class='login-login-button']"));
+		System.out.println("waitforelement");
 		driver.findElement(By.xpath(obj.getProperty("clickOnLoginBtn"))).click();
 	}
+	
+//	public static void waitforelement(By by) {
+////		WebDriver driver = null;
+//		WebDriverWait wait = new WebDriverWait(driver, 30);
+//		 wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+//		
+//	}
 
 }
